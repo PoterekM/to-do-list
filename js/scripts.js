3bb7cd57f042f24f04e97791318ba9a9afdc6b2b
Michelle Poterek and Nathan Stewart <michelle@>
2017-06-12 11:40:43 -0700

    add form for user input and set up constructor

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//business logic
+
+function List(toDo, details) {
+  this.toDo = toDo;
+  this.details = details;
+}
+
+
+
+
+
+//User Interface Logicv
+
+// $(document).ready(function() {
+//   $("form#userInput").submit(function(event) {
+//     event.preventDefault();
+//
+//
+//
+//   });
+// });
